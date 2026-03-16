b8a2207087da238a1d7754a682251634b3539770
Margherita code <125656206+Margherita-Scafa@users.noreply.github.com>
2026-03-16 22:12:58 +0100

    Delete day1_read_csv.py

--- day1_read_csv.py
+++
@@ -1,9 +0,0 @@
-file = open("week2/data/security_log.csv", "r")
-
-for riga in file:
-    riga = riga.strip()   #riga.strip() è la funzione che rimuove gli spazi iniziali e finali della stringa.
-    colonne = riga.split(",") #ho una lista con i campi della riga
-    
-    print(colonne)            
- 
-file.close()
